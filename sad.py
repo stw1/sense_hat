d1d97f7e5737dad69564c01d6550b2c58874d50e
Stephen Wong <stwong1@gmail.com>
2016-09-16 11:14:44 -0700

    Moved shutdown code to smile.py.  Added code to detect shtudown/reboot via signal command from the OS.

--- sad.py
+++ sad.py
@@ -1,38 +1,3 @@
 #!/usr/bin/python
-from sense_hat import SenseHat
-from time import sleep
-
-# set up the colours (white, green, red, empty)
-
-w = [150, 150, 150]
-g = [0, 255, 0]
-r = [255, 0, 0]
-e = [0, 0, 0]
-
-sad = [
-e,e,e,e,e,e,e,e,
-e,r,r,e,e,r,r,e,
-e,r,r,e,e,r,r,e,
-e,e,e,e,e,e,e,e,
-e,e,e,e,e,e,e,e,
-e,e,r,r,r,r,e,e,
-e,r,e,e,e,e,r,e,
-r,e,e,e,e,e,e,r
-]
-
-sense = SenseHat()
-
-sense.clear()
-
-sense.show_message("Shutdown", scroll_speed=(0.05), text_colour=(255, 0, 0), back_colour=(0, 0,
-0))
-
-sense.show_letter("3")
-sleep(1)
-sense.show_letter("2")
-sleep(1)
-sense.show_letter("1")
-sleep(1)
-sense.set_pixels(sad)
 
 exit()
